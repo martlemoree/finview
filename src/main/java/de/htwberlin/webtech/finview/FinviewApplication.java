e58814d44d1755824a70f3a1460e40780222063e
martlemoree <71133462+martlemoree@users.noreply.github.com>
2022-04-18 10:15:27 +0200

    initial commit

---
+++ src/main/java/de/htwberlin/webtech/finview/FinviewApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.finview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/finview/FinviewApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.finview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
